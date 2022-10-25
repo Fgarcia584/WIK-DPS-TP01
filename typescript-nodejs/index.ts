ce455dc6bf5a2624616d7f9493f4ca10d5905e46
unknown <fabio.garcia@ynov.com>
2022-10-25 11:44:17 +0200

    use correction to be sure the API is good

--- typescript-nodejs/index.ts
+++ typescript-nodejs/index.ts
@@ -1,31 +1,40 @@
-import express from 'express';
+import { createServer, IncomingMessage, ServerResponse } from "http"
 
-require('dotenv').config();
-
-const app = express();
-
-
-app.get('/ping', (req, res) => {
-    return res.json(req.headers);
-})
-
-app.get('*', function(req, res){
-    return res.sendStatus(404);
-});
-
-app.use('*', function(req, res){
-    return res.sendStatus(500);
-});
-
-const start = async () => {
-try {
-    var env_port: number = !process.env.PING_LISTEN_PORT
-    ? 3000
-    : +process.env.PING_LISTEN_PORT;
-    await app.listen({ port: env_port });
-} catch (err) {
-    console.log(err);
-    process.exit(1);
+// Method used to handle incoming requests
+const requestListener = function (req: IncomingMessage, res: ServerResponse) {
+  try {
+    // Only send JSON if HTTP verb is GET and url is /ping
+    if (req.method === "GET" && req.url === "/ping") {
+      res.setHeader("Content-Type", "application/json")
+      res.write(JSON.stringify(req.headers))
+      res.end();
+    // Else return HTTP 404
+    } else {
+      res.statusCode = 404
+      res.end()
+    }
+  // If something went wrong return HTTP 500
+  } catch (err) {
+    console.error(err)
+    res.statusCode = 500
+    res.end()
+  }
 }
-};
-start();
+
+try {
+  // Server creation
+  const server = createServer(requestListener);
+  server.listen(process.env.PING_LISTEN_PORT ?? 8080);
+  const serverAddressInfo = server.address()
+  if (!serverAddressInfo) {
+    throw new Error("No server address info")
+  }
+  if (typeof serverAddressInfo === 'string') {
+    console.log(`Server listening : ${serverAddressInfo}`)
+  } else {
+    console.log(`Server listening : ${serverAddressInfo.address}:${serverAddressInfo.port}`)
+  }
+} catch (err) {
+  console.error(err)
+  process.exit(1)
+}
